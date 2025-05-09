9b97cc67ba19de53a39cc3be5b9c35034a6f11e4
eden-chung <edenchung23@gmail.com>
2025-05-08 23:30:30 -0400

    no warnings ast with changes for irgen and semant

--- ast.ml
+++ ast.ml
@@ -1,95 +1,113 @@
-(* MODIFIED FROM CLASS CODE. Abstract Syntax Tree and functions for printing it*)
+(* Abstract Syntax Tree and functions for printing it *)
 
-type op = Add | Sub | Times | Divide | Equal | Neq | Less | More | And | Or
+type op = ADD | SUB | EQUAL | NEQ | LT | GT | LEQ | GEQ | AND | OR | TIMES | DIVIDE
 
-type unaryop = Not | Neg
+type unop = NOT
 
-type typ = Int | Bool | Note
+type typ = INT | NOTE | STRING | BOOL
 
-(* define how note is stored*)
 type note = {
-  pitch : string;
-  octave : int;
-  length : int;
+pitch : string;
+octave : int;
+length : int;
 }
 
 type expr =
     Literal of int
-  | BoolLit of bool
-  | NoteLit of note
-  | StringLit of string
-  | Id of string
-  | Binop of expr * op * expr
-  | Unop of unaryop * expr
-  | Assign of string * expr
-  (* function call *)
-  | Call of string * expr list (* Function call is a new type of express in microC. List of expressions passed into the function*)
+   | BoolLit   of bool
+   | StringLit of string
+   | NoteLit   of note
+   | Id        of string
+   | Binop     of expr * op * expr
+   | Unop      of unop * expr
+   | Assign    of string * expr
+   | Call      of string * expr list
 
-type stmt =
-    Block of stmt list
-  | Expr of expr
-  | If of expr * stmt * stmt
-  | While of expr * stmt
-  | For of expr * expr * expr * stmt (* for loop*)
-  | Print of expr
-  | Repeat of expr * stmt (* repeat n times loop*)
-  | Write of stmt
-  (* return *)
-  | Return of expr (* we need a return statement, it returns an expression*)
+ type stmt =
+     Block    of stmt list
+   | Expr     of expr
+   | If       of expr * stmt * stmt
+   | While    of expr * stmt
+   | For      of string * expr * stmt  (* for loop *)
+   | Print    of expr
+   | Repeat   of expr * stmt               (* repeat n times *)
+   | Return   of expr
+   | Continue 
+   | Break    
 
 (* int x: name binding *)
 type bind = typ * string
 
 (* func_def: ret_typ fname formals locals body *)
 type func_def = {
-  rtyp: typ; (* return type *)
-  fname: string; (* function name *)
-  formals: bind list; (* list of arguments, for each we need a type and a string, which is a bind*)
-  locals: bind list; (* then we have a program, which is locals and body*)
-  body: stmt list;
+   rtyp   : typ;
+   fname  : string;
+   formals: bind list;
+   locals : bind list;
+   body   : stmt list;
 }
 
-type program = bind list * func_def list (* but now a program is a list of locals and body and function definitions*)
+type program = bind list * func_def list
 
 (* Pretty-printing functions *)
 let string_of_op = function
-    Add -> "+"
-  | Sub -> "-"
-  | Equal -> "=="
-  | Neq -> "!="
-  | Less -> "<"
-  | More -> ">"
-  | Times -> "*"
-  | Divide -> "/"
-  | And -> "&&"
-  | Or -> "||"
+    ADD -> "+"
+  | SUB -> "-"
+  | EQUAL -> "=="
+  | NEQ -> "!="
+  | LEQ -> "<="
+  | GEQ -> ">="
+  | LT -> "<"
+  | GT -> ">"
+  | TIMES -> "*"
+  | DIVIDE -> "/"
+  | AND -> "AND"
+  | OR -> "OR"
+let string_of_unop = function
+  | NOT -> "NOT"
 
-let rec string_of_expr = function
-    Literal(l) -> string_of_int l
-  | BoolLit(true) -> "true"
-  | BoolLit(false) -> "false"
-  | Id(s) -> s
-  | Binop(e1, o, e2) ->
-    string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
-  | Call(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+ let rec string_of_expr = function
+     Literal l      -> string_of_int l
+   | BoolLit true  -> "TRUE"
+   | BoolLit false -> "FALSE"
+   | StringLit s -> s
+   | NoteLit n -> n.pitch ^string_of_int n.octave ^ string_of_int n.length
+   | Id s          -> s
+   | Binop (e1, o, e2) ->
+     string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+   | Unop (u, e) -> string_of_unop u ^ " " ^ string_of_expr e
+   | Assign (v, e) -> v ^ " = " ^ string_of_expr e
+   | Call (f, el)  ->
+       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
 
 let rec string_of_stmt = function
     Block(stmts) ->
     "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
   | Expr(expr) -> string_of_expr expr ^ ";\n"
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n"
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-                      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
-  | Write(stmt) -> "write " ^ string_of_stmt stmt ^ ";\n" (* Add pretty-printing for Write *)
+  | Return(expr) -> "RETURN " ^ string_of_expr expr ^ "!\n"
+  | Break -> "BREAK!\n"
+  | Continue -> "CONTINUE!\n"
+  | If(e, s1, s2) ->  "IF (" ^ string_of_expr e ^ ")\n" ^
+                      string_of_stmt s1 ^ "ELSE\n" ^ string_of_stmt s2
+  | While(e, s) -> "WHILE (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+  | For(x, y, z) -> "FOR (" ^ x ^ "IN " ^ string_of_expr y ^ ") " ^ string_of_stmt z
+  | Repeat(x, s) -> "REPEAT (" ^string_of_expr x ^ ") " ^ string_of_stmt s
+  | Print(x) -> "PRINT (" ^ string_of_expr x ^ ")!"
+  
+
+
 
 let string_of_typ = function
-    Int -> "int"
-  | Bool -> "bool"
+    INT -> "INT"
+  | BOOL -> "BOOL"
+  | NOTE -> "NOTE"
+  | STRING -> "STRING"
 
-let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ "!\n"
+
+
+let string_of_vinitialize (t, id, initialize) = string_of_typ t ^ " " ^ id ^ "=" ^ initialize ^ "!\n"
+
 
 let string_of_fdecl fdecl =
   string_of_typ fdecl.rtyp ^ " " ^
@@ -99,7 +117,8 @@ let string_of_fdecl fdecl =
   String.concat "" (List.map string_of_stmt fdecl.body) ^
   "}\n"
 
-let string_of_program (vars, funcs) =
+
+let string_of_program (vars, funcs, initialize) =
   "\n\nParsed program: \n\n" ^
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^ String.concat "" (List.map string_of_vinitialize initialize) ^ "\n" ^
   String.concat "\n" (List.map string_of_fdecl funcs)
